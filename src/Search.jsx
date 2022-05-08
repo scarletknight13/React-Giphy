e25eab9dddc20fea33c7c7d0364ed1bf03fe1a00
scarletknight13 <lamarbroxton@gmail.com>
2022-05-08 13:42:07 -0500

    adding to remote

--- src/Search.jsx
+++ src/Search.jsx
@@ -4,7 +4,7 @@ function Search(props){
     console.log(props.handleChange)
     return(
         <div>
-            <form action="" onSubmit={props.handleSubmit}>
+            <form action="">
                 <input onInput={props.handleChange} name="name" type="text" />
                 <input type="submit" value="Search"  />
             </form>
--- src/SearchContainer.jsx
+++ src/SearchContainer.jsx
@@ -5,10 +5,10 @@ const axios = require('axios');
 
 
 function SearchContainer(props){
-    function handleSubmit(event){
-        event.preventDefault();
-        console.log(event.target.name.value);
-        let searchWords = event.target.name.value.split(' ');
+
+    function handleChange(event){
+        // console.log(event.target.value);
+        let searchWords = event.target.value.split(' ');
         console.log(searchWords);
         axios.get(`https://api.giphy.com/v1/gifs/search?api_key=6uTMhTUfwlqR6vn2Sqqc1ekzbJ8dAgeW&q=${searchWords.join('+')}&limit=25&offset=0&rating=g&lang=en`)
             .then(function (response) {
@@ -16,23 +16,15 @@ function SearchContainer(props){
                     queryText: event.target.value,
                     results : response.data.data
                 })
-                console.log(query);
             })
             .catch(function (error) {
                 console.log(error);
             })
-        setQuery({
-            queryText: event.target.value,
-            results : []});
-        // console.log(event.target.name.value);
-    }
-    function handleChange(event){
-        console.log(event.target.value);
     }
     const [query, setQuery] = useState({queryText: '', results : []})
     return(
         <div>
-            <Search handleChange={handleChange} handleSubmit={handleSubmit}/>
+            <Search handleChange={handleChange}/>
             <Results data={query.results} />
         </div>
     )
